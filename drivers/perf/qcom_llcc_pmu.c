95ebdf25eaffbba4ea51b4f149edf1d5bc67de2e
Adam W. Willis <return.of.octobot@gmail.com>
2021-09-02 00:32:51 +0300

    Revert "Merge branch 'gcc/neutrino-msm-kebab-4.19' into neutrino-msm-kebab-4.19"
    Probably Op8Pro and Op8 aren't ready yet for building using
    newer GCC chains with LTO flag enabled.
    
    This reverts commit 1b1e95800b67635f2e8b5600ae5d4672431016bf, reversing
    changes made to 20ecde9feb0de6d32ae36091b5f873d3d8a02f44.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>
    Change-Id: I487ecd4d24dc3f9b23aa6cd0563be3ac254686eb

--- drivers/perf/qcom_llcc_pmu.c
+++ drivers/perf/qcom_llcc_pmu.c
@@ -116,7 +116,7 @@ static void mon_enable(struct llcc_pmu *llccpmu, int cpu)
 
 static unsigned long read_cnt(struct llcc_pmu *llccpmu, int cpu)
 {
-	unsigned long value = 0;
+	unsigned long value;
 
 	if (!llccpmu->ver) {
 		pr_err("LLCCPMU version not correct\n");
