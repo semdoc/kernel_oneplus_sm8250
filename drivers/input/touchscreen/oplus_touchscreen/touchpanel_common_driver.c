ea721f21f1213926400a6c042ef9b38050b4f479
Michael Bestas <mkbestas@lineageos.org>
2022-12-04 23:13:24 +0200

    oplus_touchscreen: Make it build
    Change-Id: I76ec77fb1964ad9c0bbb2d471c48b6bf4c1e868b

--- drivers/input/touchscreen/oplus_touchscreen/touchpanel_common_driver.c
+++ drivers/input/touchscreen/oplus_touchscreen/touchpanel_common_driver.c
@@ -1681,7 +1681,7 @@ void tp_gpio_current_leakage_handler(bool normal)
 }
 EXPORT_SYMBOL(tp_gpio_current_leakage_handler);
 
-bool tp_boot_mode_normal()
+static bool tp_boot_mode_normal()
 {
     if (!g_tp) {
         return false;
